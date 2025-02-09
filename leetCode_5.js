826a78d9284ea45826fa665a2a7d1400e5089841
poojakanjalkar <poojakanjalkar554@gmail.com>
2025-02-09 21:40:01 +0530

    single number problem

---
+++ leetCode_5.js
@@ -0,0 +1,13 @@
+function singleNumber(nums) {
+  let result = 0;
+  
+  for (let num of nums) {
+      result ^= num; 
+  }
+  
+  return result;
+}
+
+
+console.log(singleNumber([2, 2, 1]));     
+
